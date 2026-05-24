ae39c9e1167aef24f5b0b0f12b8ccb62d6a90078
enaitul <eprvleo@gmail.com>
2026-05-24 14:41:16 +0530

    Removed scroll indicator from Hero to prevent overlaps

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -130,20 +130,7 @@ export default function Hero() {
         </div>
       </div>
 
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5 }}
-        className="absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 text-white/30"
-      >
-        <span className="font-mono text-xs tracking-widest uppercase">Scroll</span>
-        <motion.div
-          animate={{ y: [0, 8, 0] }}
-          transition={{ duration: 1.5, repeat: Infinity, ease: 'easeInOut' }}
-        >
-          <FiArrowDown size={16} />
-        </motion.div>
-      </motion.div>
+
     </section>
   )
 }
